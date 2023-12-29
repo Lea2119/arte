83019663b025670c364306b306a106dd0c95d8c5
Lea Cassegrain <lea.csgn@gmail.com>
2023-12-29 22:59:44 +0100

    Frontend: project initialized and dependencies installed

---
+++ frontend/src/layouts/default/AppBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar></v-app-bar>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-6" />
+
+      Essentials Preset
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
