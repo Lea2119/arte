fcb2ff68f8668ae605f64314a095e68b57308d34
Lea Cassegrain <lea.csgn@gmail.com>
2023-12-30 00:47:24 +0100

    Frontend: appbar by defaut and for IOS created

---
+++ frontend/src/components/Appbars/Appbar.vue
@@ -0,0 +1,126 @@
+<template>
+	<div>
+		<v-app-bar
+			v-if="!props.isIphone15"
+			scroll-behavior="collapse"
+			color="#e4012c"
+		>
+			<template v-slot:prepend>
+				<v-app-bar-nav-icon
+					variant="text"
+					@click.stop="drawer = !drawer"
+				></v-app-bar-nav-icon>
+			</template>
+		</v-app-bar>
+		<v-navigation-drawer
+			class="nav-drawer w-25"
+			style="background-color: #e4012c"
+			v-model="drawer"
+			location="left"
+			temporary
+		>
+			<v-list
+				v-for="(item, index) in filteredItems"
+				:key="index"
+				density="compact"
+			>
+				<v-list-item
+					style="color: white"
+					:prepend-icon="item.icon"
+					:title="item.title"
+					:value="item.value"
+				></v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+		<v-navigation-drawer
+			class="nav-drawer w-25"
+			v-model="drawer"
+			location="left"
+			temporary
+		>
+		</v-navigation-drawer>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, watch, defineProps } from "vue"
+
+const userIsPremium = ref(false)
+const userIsAdmin = ref(true)
+const group = ref()
+const props = defineProps(["isIphone15"])
+const drawer = ref(false)
+
+interface Item {
+	title: string
+	value: string
+	icon: string
+	account?: string
+}
+
+const items = ref<Array<Item>>([
+	{
+		title: "home",
+		value: "home",
+		icon: "mdi-home",
+	},
+	{
+		title: "quizzes",
+		value: "quizzes",
+		icon: "mdi-puzzle",
+		account: "premium",
+	},
+	{
+		title: "artists",
+		value: "artists",
+		icon: "mdi-account",
+	},
+	{
+		title: "artworks",
+		value: "artworks",
+		icon: "mdi-palette",
+	},
+	{
+		title: "mediums",
+		value: "mediums",
+		icon: "mdi-brush",
+	},
+	{
+		title: "data facts",
+		value: "data facts",
+		icon: "mdi-chart-donut",
+		account: "premium",
+	},
+	{
+		title: "edit",
+		value: "edit",
+		icon: "mdi-cog",
+		account: "admin",
+	},
+	{
+		title: "account",
+		value: "account",
+		icon: "mdi-account-circle",
+		account: "admin",
+	},
+])
+
+const filteredItems = computed(() => {
+	if (userIsPremium.value) {
+		return items.value.filter((item: Item) => item.account !== "admin")
+	} else if (userIsAdmin.value) {
+		return items.value
+	} else {
+		return items.value.filter(
+			(item: Item) =>
+				item.account !== "premium" && item.account !== "admin"
+		)
+	}
+})
+
+watch(group, () => {
+	drawer.value = false
+})
+</script>
+
+<style scoped></style>
--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -1,14 +1,28 @@
 <template>
-  <v-app-bar></v-app-bar>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
-    </v-app-bar-title>
-  </v-app-bar>
+	<div>
+		<Appbar :isIphone15="isIphone15" />
+		<AppbarIOS :isIphone15="isIphone15" />
+	</div>
 </template>
 
 <script lang="ts" setup>
-//
+import { ref, onMounted } from "vue"
+import { Device } from "@capacitor/device"
+import Appbar from "@/components/Appbars/Appbar.vue"
+import AppbarIOS from "@/components/Appbars/AppbarIOS.vue"
+
+const isIphone15 = ref(false)
+
+onMounted(async () => {
+	const deviceInfo = await Device.getInfo()
+	const iphone15 = deviceInfo.model.toLowerCase().includes("iphone15")
+	isIphone15.value = iphone15
+})
 </script>
+
+<style scoped>
+.nav-drawer {
+	color: #e4012c;
+	font-weight: bold;
+}
+</style>
